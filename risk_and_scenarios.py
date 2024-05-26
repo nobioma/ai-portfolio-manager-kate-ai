68641c8f2a985fc3a9db172e811e7dd0f9015ab7
Nnanyere Obioma <127893939+nobioma@users.noreply.github.com>
2024-05-26 18:13:32 -0400

    Add files via upload

---
+++ risk_and_scenarios.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import plotly.express as px
+import altair as alt
+import pandas as pd
+
+st.title('Risk and Scenerios')
+
+while 'risk' not in st.session_state:
+    pass
+
+st.write(st.session_state['risk'])
+
